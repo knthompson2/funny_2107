b408444096bc242300cd65d6234e7c51b764d63e
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-14 17:55:44 -0400

    Complete implementation for open mic class

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,7 +1,7 @@
+require './lib/joke'
+require './lib/user'
 class OpenMic
-  attr_reader :location,
-              :date,
-              :performers
+  attr_reader :location, :date, :performers
 
   def initialize(attributes)
     @location = attributes[:location]
@@ -13,10 +13,31 @@ class OpenMic
     @performers << user
   end
 
-  def repeated_jokes?
-    jokes = @performers.map do |user|
-      user.jokes
-    end.flatten
-    jokes.length != jokes.uniq.length
-  end
+  # def repeated_jokes?
+  #   find_jokes = @performers.each do |user|
+  #     user.jokes
+  #   end
+  #   potential_repeat_jokes = find_jokes.each_with_object(Hash.new(0)) { |joke,counts| counts[joke] += 1}
+  # 
+  #   potential_repeat_jokes.each do |joke|
+  #     if potential_repeat_jokes[counts] > 1
+  #       true
+  #     else
+  #       false
+  #     end
+  #   end
+  #
+  #   # potential_repeat_jokes = Hash.new(0)
+  #   # find_jokes.each do |joke|
+  #   #   potential_repeat_jokes[joke] += 1
+  #   # end
+  #   #
+  #   # potential_repeat_jokes.each do |k, v|
+  #   #   if v > 1
+  #   #     true
+  #   #   else
+  #   #     false
+  #   #   end
+  #   potential_repeat_jokes
+  # end
 end
